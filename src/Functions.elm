9807c9ccc02f42030ed22b7de2c263e3b1ea8d5d
moss <moss@tvisioninsights.com>
2017-08-12 18:09:40 -0400

    Some functions
    We’ll attempt to apply these to the Lens types soon.

---
+++ src/Functions.elm
@@ -0,0 +1,73 @@
+module Functions exposing (..)
+
+import Haskell exposing (..)
+import Lens exposing (s, t, a, b, applicative)
+
+
+monad =
+    TypeClass { name = "Monad", supers = [ applicative ] }
+
+
+monadReader =
+    TypeClass { name = "MonadReader", supers = [ monad ] }
+
+
+monadState =
+    TypeClass { name = "MonadState", supers = [ monad ] }
+
+
+m =
+    TypeVar { name = "m" }
+
+
+r =
+    TypeVar { name = "r" }
+
+
+const =
+    TypeConstructor { name = "Const" }
+
+
+identity =
+    TypeConstructor { name = "Identity" }
+
+
+getting =
+    TypeAlias
+        { name = "Getting"
+        , args = [ r, s, a ]
+        , rhs =
+            (Fn (Fn (Var a) (app (Constr const) [ (Var r), (Var a) ]))
+                (Fn (Var s) (app (Constr const) [ (Var r), (Var s) ]))
+            )
+        }
+
+
+aSetter =
+    TypeAlias
+        { name = "ASetter"
+        , args = [ s, t, a, b ]
+        , rhs =
+            (Fn (Fn (Var a) (App (Constr identity) (Var b)))
+                (Fn (Var s) (App (Constr identity) (Var t)))
+            )
+        }
+
+
+functions : List ( String, Type )
+functions =
+    [ ( "view"
+      , Constrained [ TypeClassConstraint monadReader [ m ] ]
+            (Fn (app (aliasRef getting) [ (Var a), (Var s), (Var a) ]) (App (Var m) (Var a)))
+      )
+    , ( "set"
+      , Fn (app (aliasRef aSetter) [ (Var s), (Var t), (Var a), (Var b) ]) (Fn (Var s) (Var t))
+      )
+    , ( "over"
+      , Fn (app (aliasRef aSetter) [ (Var s), (Var t), (Var a), (Var b) ]) (Fn (Fn (Var a) (Var b)) (Fn (Var s) (Var t)))
+      )
+    , ( "assign"
+      , Constrained [ TypeClassConstraint monadState [ s, m ] ]
+            (Fn (app (aliasRef aSetter) [ (Var s), (Var t), (Var a), (Var b) ]) (Fn (Var b) (App (Var m) Unit)))
+      )
+    ]
